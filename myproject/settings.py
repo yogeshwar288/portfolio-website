127129f8b00160a4418b92e201c1fab9cb0e8d45
yogeshwar khandagre <67906182+yogeshwar288@users.noreply.github.com>
2022-12-29 22:42:29 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR=[os.path.join(BASE_DIR,"templates")]
+
 # BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
